5d95e2fbd26ed390c111ee052b6a0bf6926695c6
Diogo Grings Graebin <diogograebin@gmail.com>
2025-09-24 22:53:05 -0300

    Adiciona exemplo de estrutura usando map

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,36 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([
+    { id: 1, text: 'Estudar React', category: "Estudos", isCompleted: false },
+    { id: 2, text: 'Implementar nova tela', category: "Trabalho", isCompleted: false },
+    { id: 3, text: 'Estudar Inglês', category: "Estudos", isCompleted: false },
+  ]);
+
+  console.log(todos);
 
   return (
     <>
-      Olá React
+      <div className='app'>
+        <h1>Lista de Tarefas</h1>
+        <div className="todo-list">
+          {todos.map((todo) => (
+            <div className="todo">
+              <div className="content">
+                <p>{todo.text}</p>
+                <p className="category">
+                  ({todo.category})
+                </p>
+              </div>
+
+              <div>
+                <button>Completar</button>
+                <button>x</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </>
   )
 }
